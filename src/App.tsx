d36feecc9f4af35d88c60d49b9ed0c255efbb7be
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2024-12-03 15:09:46 +0300

    attrs

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,52 @@
-import './App.css';
-import styled from 'styled-components';
-
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
-    return (
-        <div className="App">
-            <Title>Styled-components <span>.attrs</span> method</Title>
+  return (
+    <div className="App">
+      <Title>
+        Styled-components <span>.attrs</span> method
+      </Title>
 
-            <Form>
-                <Field/>
-                <Field/>
-                <Field/>
-            </Form>
-        </div>
-    );
+      <Form>
+        <Field type="password" placeholder="Heelo"/>
+        <Field type="text" placeholder="Hello"/>
+        <Field type="text" />
+        <Checkbox />
+      </Form>
+    </div>
+  );
 }
 
 export default App;
 
 const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    margin-bottom: 32px;
-    span {
-      color: #e91e63;
-    }
-`
+  font-size: 1.5em;
+  text-align: center;
+  margin-bottom: 32px;
+  span {
+    color: #e91e63;
+  }
+`;
 
 const Form = styled.form`
   width: 100%;
   max-width: 500px;
 `;
 
-const Field = styled.input`
+const Field = styled.input.attrs(({placeholder})=>{
+  return{
+    placeholder: placeholder || "Type smth..."
+  }
+    })`
   padding: 5px 15px;
   margin: 10px 0;
   width: 100%;
   font-size: 1rem;
 `;
+
+const Checkbox = styled.input.attrs(() => {
+  return {
+    type: "checkbox",
+  };
+})``;
